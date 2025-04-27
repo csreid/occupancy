04e68e70180cf526fe1f87ab736dc73322e6074c
Cameron Reid <csreid.cr@gmail.com>
2025-04-27 18:13:56 -0500

    Apply sigmoid to logits

--- train.py
+++ train.py
@@ -45,7 +45,13 @@ def validate(model):
 def log_sample(i):
 	(sample_imgs, sample_scans, sample_init_pose), (grids, poses) = loader.sample(1, for_cv=False)
 	with torch.no_grad():
-		sample_out = model(sample_scans.to(dev), sample_imgs.to(dev), sample_init_pose.to(dev))[0].squeeze().unsqueeze(0).unsqueeze(2).to(dev)
+		sample_out = torch.sigmoid(
+				model(
+					sample_scans.to(dev),
+					sample_imgs.to(dev),
+					sample_init_pose.to(dev)
+				)[0].squeeze().unsqueeze(0).unsqueeze(2).to(dev)
+		)
 		grids = grids.squeeze().unsqueeze(0).unsqueeze(2).to(dev)
 
 		video = torch.cat((grids, sample_out), dim=-1).expand(-1, -1, 3, -1, -1) # Pop the one channel into RGB to treat it like a video
